cae70e4eb1213797889df97ade2757ecd02e666d
ETAILZ.COM\MichaelC <MichaelC@etailz.com>
2018-10-25 14:10:57 -0700

    no message

---
+++ PHP/variables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+
+    <?php 
+
+    echo "Anything";
+
+    ?>
+
+  </body>
+</html>
