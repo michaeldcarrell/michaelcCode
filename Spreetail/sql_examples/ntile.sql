4a4763cd6fdd4cbbe5e93247a0ebf4a1a1b56675
michaeldcarrell <michaeldcarrell@gmail.com>
2019-01-24 14:28:49 -0600

    Initial Commit to git

--- Spreetail/sql_examples/ntile.sql
+++
@@ -1,2 +0,0 @@
-select i.PartNum, i.Cost, ntile(100) over (order by i.Cost) Percentile from btdata.dbo.Inventory i
-order by Percentile desc
--- Spreetail/sql_examples/partitioning.sql
+++
@@ -1,5 +0,0 @@
-select i.PartNum,
-       i.Cost,
-       i.SupplierID,
-       dense_rank() over (partition by i.SupplierID order by i.Cost) SupplierRank
-from btdata.dbo.Inventory i
